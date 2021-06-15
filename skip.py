fc8d07eda5ba5f1bcc37d4ea9975082563cbadfe
Harry <hme7@georgetown.edu>
2021-06-15 19:31:25 -0400

    Add a file watcher for reloads

--- skip.py
+++ skip.py
@@ -1,9 +1,12 @@
+import asyncio
 import pkgutil
+
+from watchgod import awatch
 
 import _data
 
-if __name__ == "__main__":
 
+def process_data():
     data_modules = []
     for loader, module_name, is_pkg in pkgutil.iter_modules(_data.__path__):
         module = loader.find_module(module_name).load_module(module_name)
@@ -11,3 +14,14 @@ if __name__ == "__main__":
 
     for module in data_modules:
         print(module.get_data())
+
+
+async def main():
+    async for changes in awatch("_data"):
+        process_data()
+
+
+if __name__ == "__main__":
+    process_data()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
