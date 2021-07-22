8e779227e4821249351cb9b91bf2346a586631bd
Harry <hme7@georgetown.edu>
2021-07-22 14:10:14 -0400

    Make homepage responsive

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '188': '47rem'
+      }
+    },
   },
   variants: {
     extend: {},
