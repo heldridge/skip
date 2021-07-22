a6bef7b2fd0ddcf25da8f28955bf397b47e8db92
Harry <hme7@georgetown.edu>
2021-07-22 11:24:37 -0400

    Add tailwindcss to docs

---
+++ docs/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./**/*.j2"],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
